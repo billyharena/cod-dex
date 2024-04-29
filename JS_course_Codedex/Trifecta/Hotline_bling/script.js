84744d967545dafd02d9f8920ebeec021b136d7f
billyharena <billy.harena@gmail.com>
2024-04-29 18:48:27 +0300

    JS course
    These are some JS exercises.

---
+++ JS_course_Codedex/Trifecta/Hotline_bling/script.js
@@ -0,0 +1,12 @@
+// let drakePicTwo = document.getElementById("drake-pic-2");
+// drakePicTwo.src = "https://i.imgur.com/RGbh6zY.png";
+// let heading = document.getElementById("heading-2");
+// heading.innerText = "Learn By Doing";
+
+
+function change(){
+    let drakePicTwo = document.querySelector("#drake-pic-2");
+    drakePicTwo.src = "https://i.imgur.com/RGbh6zY.png";
+    let heading = document.querySelector("#heading-2");
+    heading.innerText = "BASTA BABY"
+}
